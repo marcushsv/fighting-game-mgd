f23dea206b643eee349d8cdcc759e73c1c4177dc
Ron <ronny@manjang.de>
2023-05-14 21:13:14 +0100

    animation tree map, hit detect

--- godot/playerwasdmovement.gd
+++ godot/playerwasdmovement.gd
@@ -3,12 +3,15 @@ extends CharacterBody2D
 
 @export var speed : float  = 300.0
 @export var jump_velocity : float = -400.0
-@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
-var animation_locked : bool = false
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 var direction : Vector2 = Vector2.ZERO
+var state_machine
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	state_machine = $AnimationTree.get("parameters/playback")
 
 
 func _physics_process(delta):
@@ -19,9 +22,10 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("w") and is_on_floor():
 		velocity.y = jump_velocity
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		
+	if Input.is_action_just_pressed("space"):
+		attack()
+	
 	direction = Input.get_vector("a", "d", "w", "s")
 	if direction:
 		velocity.x = direction.x * speed
@@ -29,19 +33,17 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
-	updateAnimation()
 	updateFacingDirection()
-	
-	
-func updateAnimation():
-	if not animation_locked:
-		if direction.x != 0:
-			animated_sprite.play("run")
-		else:
-			animated_sprite.play("idle")
 
 func updateFacingDirection():
 	if direction.x > 0:
-		animated_sprite.flip_h = false
+		sprite.flip_h = false
 	elif direction.x < 0:
-		animated_sprite.flip_h = true
+		sprite.flip_h = true
+
+func attack():
+	state_machine.travel("slasher")
+
+func _on_sword_hit_area_entered(area):
+	if area.is_in_group("hurtbox"):
+		area.take_damage()
