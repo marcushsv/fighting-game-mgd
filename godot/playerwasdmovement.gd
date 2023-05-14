a33e6738e65f0453c87af4569e1842f9f947b080
Ron <ronny@manjang.de>
2023-05-14 21:18:09 +0100

    updated animationtree

--- godot/playerwasdmovement.gd
+++ godot/playerwasdmovement.gd
@@ -19,7 +19,6 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle Jump.
 	if Input.is_action_just_pressed("w") and is_on_floor():
 		velocity.y = jump_velocity
 		
